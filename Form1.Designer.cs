4e0f16ddcf4c28b00b4daacc09cb4194ef9edecf
YushaBinArif <youshaarif13@gmail.com>
2017-03-21 22:04:43 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApplication13
+{
+    partial class VPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VPlayer));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.wmplayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.wmplayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Speak";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(199, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 37);
+            this.textBox1.TabIndex = 2;
+            // 
+            // wmplayer
+            // 
+            this.wmplayer.Enabled = true;
+            this.wmplayer.Location = new System.Drawing.Point(11, 13);
+            this.wmplayer.Name = "wmplayer";
+            this.wmplayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmplayer.OcxState")));
+            this.wmplayer.Size = new System.Drawing.Size(609, 377);
+            this.wmplayer.TabIndex = 3;
+            this.wmplayer.Visible = false;
+            // 
+            // VPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 402);
+            this.Controls.Add(this.wmplayer);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "VPlayer";
+            this.Text = "VPlayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wmplayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private AxWMPLib.AxWindowsMediaPlayer wmplayer;
+    }
+}
+
